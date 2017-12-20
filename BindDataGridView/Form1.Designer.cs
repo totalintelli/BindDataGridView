05f80687b2e2e00fa2ba06422ec49d8855af9412
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-20 18:34:47 +0900

    first commit

---
+++ BindDataGridView/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BindDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "With Codeing";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 310);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 539);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BindGridView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+    }
+}
+
---
+++ BindDataGridView/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BindDataGridView
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\BindDataGridView\BindDataGridView\Database1.mdf;Integrated Security=True");
+        SqlDataAdapter da;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 콤보박스 추가
+            DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+            // 체크박스 추가
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            // 버튼 추가
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+
+            comboBoxColumn.HeaderText = "TestCombo";
+            comboBoxColumn.Name = "combo";
+            comboBoxColumn.Items.AddRange("Test1", "Test2", "Test3", "Test4", "Test5");
+
+            checkBoxColumn.HeaderText = "Check";
+            checkBoxColumn.Name = "check";
+
+            buttonColumn.HeaderText = "Button";
+            buttonColumn.Name = "button";
+
+            dataGridView1.Columns.Add("column0", "Number");
+            dataGridView1.Columns.Add("column1", "FileName");
+            dataGridView1.Columns.Add("column2", "FileSize");
+            dataGridView1.Columns.Add("column3", "FileExtension");
+            dataGridView1.Columns.Add("column4", "LastWriteTime");
+            dataGridView1.Columns.Add(comboBoxColumn);
+            dataGridView1.Columns.Add(checkBoxColumn);
+            dataGridView1.Columns.Add(buttonColumn);
+
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 80;
+            dataGridView1.Columns[3].Width = 80;
+            dataGridView1.Columns[4].Width = 100;
+            dataGridView1.Columns[5].Width = 80;
+            dataGridView1.Columns[6].Width = 50;
+            dataGridView1.Columns[7].Width = 50;
+
+            dataGridView1.AllowUserToAddRows = false;
+        }
+    }
+}
